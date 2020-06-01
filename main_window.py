abb07da3c1a910bc0b53cd5d3c255c70adb416a9
zzzX1235 <kaiyang1998@outlook.com>
2020-06-01 18:07:59 +0800

    Update

--- main_window.py
+++ main_window.py
@@ -15,3 +15,14 @@ Copyright 2020 Taoidle
    limitations under the License.
 
 """
+from PyQt5.QtWidgets import QWidget
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        pass
+
